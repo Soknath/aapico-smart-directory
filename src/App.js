7d636048a559d0758a9fc018b01760f1cdaabbae
Tori Pope <toricpope@gmail.com>
2019-02-24 19:37:39 -0600

    setting up react app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 import './App.css';
+
+const NavBar = () => (
+  <div className="navbar">
+    <h3>Task Manager</h3>
+    <Link to="/">Current Tasks</Link>
+    <Link to="/profile">Completed Tasks</Link>
+  </div>
+);
+
+const Template = (props) => (
+  <div>
+    <NavBar />
+    <p className="page-info">
+      {props.title}:
+      <ul className={props.status}>
+        <li>Task 1</li>
+        <li>Task 2</li>
+        <li>Task 3</li>
+      </ul>
+    </p>
+  </div>
+);
+
+const CurrentTasks = () => (
+  <Template title="Current Tasks" status="Current"/>
+);
+
+const CompletedTasks = () => (
+  <Template title="Completed Tasks" status="Completed"/>
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={CurrentTasks}/>
+          <Route path="/profile" component={CompletedTasks}/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
